44ce5e8268f2eee53d4f9fc6fa9928731d0ce176
Rodro <rodro@Rodros-MacBook-Air.local>
2024-04-05 09:25:45 +0600

    database added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@car-service.rhyxtaz.mongodb.net/?retryWrites=true&w=majority&appName=car-service`;
 
-console.log(uri)
+// console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,12 +27,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('carMaintain').collection('services')
+
+    app.get('/services', async(req,res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
